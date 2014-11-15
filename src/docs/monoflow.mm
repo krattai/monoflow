c5cd578199029cf483d0ca8ad039354fc1443797
Kevin Rattai <krattai@gmail.com>
2014-11-14 22:12:03 -0600

    Added monoflow mind map

---
+++ src/docs/monoflow.mm
@@ -0,0 +1,65 @@
+<map version="freeplane 1.2.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Monoflow system" ID="Freemind_Link_1043402355" CREATED="1269462110846" MODIFIED="1416022690954">
+<icon BUILTIN="gohome"/>
+<hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="core" POSITION="right" ID="_" CREATED="1269462169926" MODIFIED="1416022897702" HGAP="40" VSHIFT="9">
+<node TEXT="Prototype Development" ID="Freemind_Link_1948237358" CREATED="1269462283400" MODIFIED="1269462296310"/>
+<node TEXT="Documentation" ID="Freemind_Link_1252698133" CREATED="1269623451636" MODIFIED="1416022927273">
+<node TEXT="core code flowcharts" ID="ID_1206235571" CREATED="1414927030668" MODIFIED="1414927085993"/>
+<node TEXT="administration guide" ID="ID_304867297" CREATED="1414927095479" MODIFIED="1414927105226"/>
+<node TEXT="user manual" ID="ID_1781367384" CREATED="1414927109026" MODIFIED="1414927114227"/>
+</node>
+</node>
+<node TEXT="interfaces" POSITION="right" ID="ID_830569553" CREATED="1414926214172" MODIFIED="1416022893471" HGAP="30" VSHIFT="90"/>
+<node TEXT="installer" POSITION="right" ID="ID_1003427674" CREATED="1414926366947" MODIFIED="1416022789102" HGAP="-300" VSHIFT="-60"/>
+</node>
+</map>
